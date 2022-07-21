36ba8d26e85e6d8cad2625177d62444fdb3adb23
Prajyu Ms <prajyu2003@gmail.com>
2022-07-21 12:50:48 +0530

    linkshrink v1.0.01

--- api.js
+++ api.js
@@ -1,9 +1,8 @@
+// require("dotenv").config();
 const express = require("express");
 const { crc32 } = require("crc");
 const { MongoClient } = require("mongodb");
 const { URL } = require("url");
-
-const url = "mongodb://localhost:27017";
 
 const dbName = "absoluteUrls";
 
@@ -15,11 +14,22 @@ app.use(express.urlencoded({ extended: true }));
 let collection = null;
 
 let main = async () => {
-  const client = new MongoClient(process.env.URL || url);
-  await client.connect();
-  const db = client.db(dbName);
-  collection = db.collection("urls");
-  const deleteResult = await collection.deleteMany({});
+  let url = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clusterdata.mptpf.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
+  if (!process.env.USER || !process.env.PASSWORD || !process.env.DATABASE) {
+    url = "mongodb://localhost:27017";
+    const client = new MongoClient(url);
+    await client.connect();
+    const db = client.db(dbName);
+    collection = db.collection("urls");
+    const deleteResult = await collection.deleteMany({});
+  } else {
+    const client = new MongoClient(url);
+    let handler = await client.connect();
+    let db = handler.db("urls");
+    collection = db.collection("urls");
+    const deleteResult = await collection.deleteMany({});
+    console.log(deleteResult);
+  }
 };
 
 main();
@@ -27,7 +37,7 @@ main();
 app.get("/stretch/:shortUrl", async (req, res) => {
   let { shortUrl } = req.params;
   let result = await findUrl(shortUrl);
-  if (result.length > 0) return res.json({ url: result[0]?.url });
+  if (result.length > 0) return res.redirect(result[0]?.url);
   else res.json({ 404: "no URL found" });
 });
 
